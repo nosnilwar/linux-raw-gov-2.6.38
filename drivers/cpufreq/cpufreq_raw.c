82db33d2a463ab6d699aeddb8745a2b41dca62b3
nosnilwar <nosnilwar@gmail.com>
2014-07-28 14:06:32 -0400

    Feito novos ajustes para criar uma workqueue para acordar quando necessário e alterado a sua prioridade para RT.

--- drivers/cpufreq/cpufreq_raw.c
+++ drivers/cpufreq/cpufreq_raw.c
@@ -4,52 +4,46 @@
 
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/smp.h>
 #include <linux/init.h>
-#include <linux/spinlock.h>
-#include <linux/interrupt.h>
 #include <linux/cpufreq.h>
 #include <linux/cpu.h>
-#include <linux/types.h>
-#include <linux/fs.h>
-#include <linux/sysfs.h>
+#include <linux/jiffies.h>
+#include <linux/kernel_stat.h>
 #include <linux/mutex.h>
+#include <linux/hrtimer.h>
+#include <linux/tick.h>
+#include <linux/ktime.h>
+#include <linux/sched.h>
 
-/**
- * A few values needed by the raw governor
- */
-static DEFINE_PER_CPU(unsigned int, cpu_max_freq);
-static DEFINE_PER_CPU(unsigned int, cpu_min_freq);
-static DEFINE_PER_CPU(unsigned int, cpu_cur_freq); /* current CPU freq */
-static DEFINE_PER_CPU(unsigned int, cpu_set_freq); /* CPU freq desired by raw */
-static DEFINE_PER_CPU(unsigned int, cpu_is_managed);
+#define POLL_TIME 100000 /* in µs */
+
+struct raw_gov_info_struct {
+	unsigned long busy_spus;	/* fixed-point */
+	struct cpufreq_policy *policy;
+	struct work_struct work;
+	unsigned int poll_int;		/* µs */
+	int flagDefinicaoPrioWorkqueue; /* verificar se foi atribuida a Prioridade RT para a Workqueue. 1 - Sim / 0 - Nao */
+
+	/*
+	 * percpu mutex that serializes governor limit change with
+	 * do_dbs_timer invocation. We do not want do_dbs_timer to run
+	 * when user is changing the governor or limits.
+	 */
+	struct mutex timer_mutex;
+};
+
+static DEFINE_PER_CPU(struct raw_gov_info_struct, raw_gov_info);
 
 static DEFINE_MUTEX(raw_mutex);
 
-static int cpus_using_raw_governor;
+static struct workqueue_struct	*kraw_wq;
+
+struct task_struct *tarefa_sinalizada;
+unsigned long long deadline_tarefa_sinalizada;
 
 struct cpufreq_frequency_table *freq_table;
 
-#define dprintk(msg...) \
-	cpufreq_debug_printk(CPUFREQ_DEBUG_GOVERNOR, "raw", msg)
-
-/* keep track of frequency transitions */
-static int raw_cpufreq_notifier(struct notifier_block *nb, unsigned long val, void *data)
-{
-	struct cpufreq_freqs *freq = data;
-
-	if (!per_cpu(cpu_is_managed, freq->cpu))
-		return 0;
-
-	dprintk("DEBUG:RAWLINSON - saving cpu_cur_freq of cpu %u to be %u kHz\n", freq->cpu, freq->new);
-	per_cpu(cpu_cur_freq, freq->cpu) = freq->new;
-
-	return 0;
-}
-
-static struct notifier_block raw_cpufreq_notifier_block = {
-	.notifier_call  = raw_cpufreq_notifier
-};
+#define dprintk(msg...) cpufreq_debug_printk(CPUFREQ_DEBUG_GOVERNOR, "raw", msg)
 
 unsigned int get_frequency_table_target(struct cpufreq_policy *policy, unsigned int target_freq)
 {
@@ -82,6 +76,27 @@ unsigned int get_frequency_table_target(struct cpufreq_policy *policy, unsigned 
 }
 
 /**
+ * SINALIZA PARA O RAW MONITOR QUE O TAREFA PREEMPTADA VOLTOU A EXECUCAO.
+ */
+static int wake_up_kworker(struct cpufreq_policy *policy, struct task_struct *task, unsigned long long deadline_ns)
+{
+	//unsigned int valid_freq = 0;
+	int ret = -EINVAL;
+	struct raw_gov_info_struct *info;
+
+	info = &per_cpu(raw_gov_info, policy->cpu);
+
+	mutex_lock(&raw_mutex);
+
+	tarefa_sinalizada = task;
+	printk("DEBUG:RAWLINSON - RAW GOVERNOR - wake_up_kworker PID (%d) -> Deadline(%llu)\n", tarefa_sinalizada->pid, deadline_ns);
+	queue_work_on(info->policy->cpu, kraw_wq, &info->work);
+
+	mutex_unlock(&raw_mutex);
+	return ret;
+}
+
+/**
  * Sets the CPU frequency to freq.
  */
 static int set_frequency(struct cpufreq_policy *policy, struct task_struct *task, unsigned int freq)
@@ -90,8 +105,6 @@ static int set_frequency(struct cpufreq_policy *policy, struct task_struct *task
 	int ret = -EINVAL;
 
 	mutex_lock(&raw_mutex);
-	if (!per_cpu(cpu_is_managed, policy->cpu))
-		goto err;
 
 	// Se alguma frequencia foi definida... então o monitor não precisa mais verificar a tarefa que foi sinalizada... \o/
 	if(task && task->pid > 0)
@@ -110,14 +123,14 @@ static int set_frequency(struct cpufreq_policy *policy, struct task_struct *task
 	 *         cpufreq_governor_raw (lock raw_mutex)
 	 */
 	valid_freq = get_frequency_table_target(policy, freq);
-	ret = __cpufreq_driver_target(policy, valid_freq, CPUFREQ_RELATION_H);
+	if(valid_freq != policy->cur)
+		ret = __cpufreq_driver_target(policy, valid_freq, CPUFREQ_RELATION_H);
 
 	//Atualizando a frequencia da tarefa para uma frequencia valida.
 	task->cpu_frequency = policy->cur; // (KHz)
 
 	printk("DEBUG:RAWLINSON - RAW GOVERNOR - set_frequency(%u) for cpu %u - %u KHz - GOV(%s) -> PID (%d)\n", freq, policy->cpu, policy->cur, policy->governor->name, task->pid);
 
-err:
 	mutex_unlock(&raw_mutex);
 	return ret;
 }
@@ -131,90 +144,126 @@ static int cpufreq_raw_set(struct cpufreq_policy *policy, unsigned int freq)
 	int ret = -EINVAL;
 
 	mutex_lock(&raw_mutex);
-	if (!per_cpu(cpu_is_managed, policy->cpu))
-		goto err;
 
 	valid_freq = get_frequency_table_target(policy, freq);
-	ret = __cpufreq_driver_target(policy, valid_freq, CPUFREQ_RELATION_H);
+	if(valid_freq != policy->cur)
+		ret = __cpufreq_driver_target(policy, valid_freq, CPUFREQ_RELATION_H);
 
 	printk("DEBUG:RAWLINSON - cpufreq_raw_set(%u) for cpu %u, freq %u kHz\n", freq, policy->cpu, policy->cur);
 
-err:
 	mutex_unlock(&raw_mutex);
 	return ret;
+}
+
+static int calc_freq(struct raw_gov_info_struct *info)
+{
+	return 800000;
+}
+
+static void raw_gov_work(struct work_struct *work)
+{
+	static struct sched_param param = { .sched_priority = MAX_RT_PRIO-1 };
+	struct task_struct *task_cur;
+
+	struct raw_gov_info_struct *info;
+//	int delay;
+	unsigned long target_freq;
+
+	info = container_of(work, struct raw_gov_info_struct, work);
+
+	//*********** BEGIN: Definindo a prioridade do RAW monitor...
+	if(!info->flagDefinicaoPrioWorkqueue)
+	{
+		task_cur = get_current_task(info->policy->cpu);
+		sched_setscheduler(task_cur, SCHED_FIFO, &param);
+		printk("DEBUG:RAWLINSON - raw_gov_init_work - Passou pela definição de prioridades da Kraw. PID(%d) CPUID(%d)\n", task_cur->pid, info->policy->cpu);
+
+		info->flagDefinicaoPrioWorkqueue = 1;
+	}
+	//*********** END: Definindo a prioridade do RAW monitor...
+
+	mutex_lock(&info->timer_mutex);
+
+	if(tarefa_sinalizada && tarefa_sinalizada->pid > 0)
+	{
+		tarefa_sinalizada->flagReturnPreemption = 0;
+	}
+
+	target_freq = calc_freq(info);
+//	if(target_freq != info->policy->cur)
+//		__cpufreq_driver_target(info->policy, target_freq, CPUFREQ_RELATION_H);
+	printk("DEBUG:RAWLINSON - raw_gov_work(%lu) for cpu %u, freq %u kHz\n", target_freq, info->policy->cpu, info->policy->cur);
+//	delay = usecs_to_jiffies(info->poll_int);
+//	queue_delayed_work_on(info->policy->cpu, kraw_wq, &info->work, delay);
+
+	mutex_unlock(&info->timer_mutex);
+}
+
+static void raw_gov_init_work(struct raw_gov_info_struct *info)
+{
+//	int delay = usecs_to_jiffies(info->poll_int);
+//	INIT_DELAYED_WORK_DEFERRABLE(&info->work, raw_gov_work);
+//	queue_delayed_work_on(info->policy->cpu, kraw_wq, &info->work, delay);
+
+	INIT_WORK(&info->work, raw_gov_work);
+	queue_work_on(info->policy->cpu, kraw_wq, &info->work);
+}
+
+static void raw_gov_cancel_work(struct raw_gov_info_struct *info)
+{
+//	cancel_delayed_work_sync(&info->work);
+	cancel_work_sync(&info->work);
 }
 
 static int cpufreq_governor_raw(struct cpufreq_policy *policy, unsigned int event)
 {
 	unsigned int cpu = policy->cpu;
+	struct raw_gov_info_struct *info, *affected_info;
+	int i;
 	int rc = 0;
 
 	freq_table = cpufreq_frequency_get_table(cpu);
+	info = &per_cpu(raw_gov_info, cpu);
 
 	switch (event) {
 		case CPUFREQ_GOV_START:
 			if (!cpu_online(cpu))
 				return -EINVAL;
 
-			BUG_ON(!policy->cur);
-			mutex_lock(&raw_mutex);
-
-			if (cpus_using_raw_governor == 0) {
-				cpufreq_register_notifier(&raw_cpufreq_notifier_block, CPUFREQ_TRANSITION_NOTIFIER);
+			/* initialize raw_gov_info for all affected cpus */
+			for_each_cpu(i, policy->cpus) {
+				affected_info = &per_cpu(raw_gov_info, i);
+				affected_info->policy = policy;
 			}
-			cpus_using_raw_governor++;
 
-			per_cpu(cpu_is_managed, cpu) = 1;
-			per_cpu(cpu_min_freq, cpu) = policy->min;
-			per_cpu(cpu_max_freq, cpu) = policy->max;
-			per_cpu(cpu_cur_freq, cpu) = policy->cur;
-			per_cpu(cpu_set_freq, cpu) = policy->cur;
-			dprintk("DEBUG:RAWLINSON - managing cpu %u started "
-				"(%u - %u kHz, currently %u kHz)\n",
-					cpu,
-					per_cpu(cpu_min_freq, cpu),
-					per_cpu(cpu_max_freq, cpu),
-					per_cpu(cpu_cur_freq, cpu));
+			BUG_ON(!policy->cur);
 
-			mutex_unlock(&raw_mutex);
+			info->poll_int = POLL_TIME;
+			info->flagDefinicaoPrioWorkqueue = 0;
+
+			/* setup timer */
+			mutex_init(&info->timer_mutex);
+			raw_gov_init_work(info);
 		break;
 
 		case CPUFREQ_GOV_STOP:
-			mutex_lock(&raw_mutex);
-			cpus_using_raw_governor--;
-			if (cpus_using_raw_governor == 0) {
-				cpufreq_unregister_notifier(
-						&raw_cpufreq_notifier_block,
-						CPUFREQ_TRANSITION_NOTIFIER);
-			}
+			/* cancel timer */
+			raw_gov_cancel_work(info);
+			mutex_destroy(&info->timer_mutex);
 
-			per_cpu(cpu_is_managed, cpu) = 0;
-			per_cpu(cpu_min_freq, cpu) = 0;
-			per_cpu(cpu_max_freq, cpu) = 0;
-			per_cpu(cpu_set_freq, cpu) = 0;
-			dprintk("DEBUG:RAWLINSON - managing cpu %u stopped\n", cpu);
-			mutex_unlock(&raw_mutex);
+			/* clean raw_gov_info for all affected cpus */
+			for_each_cpu (i, policy->cpus) {
+				info = &per_cpu(raw_gov_info, i);
+				info->policy = NULL;
+			}
 		break;
 
 		case CPUFREQ_GOV_LIMITS:
 			mutex_lock(&raw_mutex);
-			dprintk("DEBUG:RAWLINSON - limit event for cpu %u: %u - %u kHz, "
-				"currently %u kHz, last set to %u kHz\n",
-				cpu, policy->min, policy->max,
-				per_cpu(cpu_cur_freq, cpu),
-				per_cpu(cpu_set_freq, cpu));
-
-			if (policy->max < per_cpu(cpu_set_freq, cpu)) {
-				__cpufreq_driver_target(policy, policy->max, CPUFREQ_RELATION_H);
-			} else if (policy->min > per_cpu(cpu_set_freq, cpu)) {
-				__cpufreq_driver_target(policy, policy->min, CPUFREQ_RELATION_L);
-			} else {
-				__cpufreq_driver_target(policy, per_cpu(cpu_set_freq, cpu), CPUFREQ_RELATION_L);
-			}
-
-			per_cpu(cpu_min_freq, cpu) = policy->min;
-			per_cpu(cpu_max_freq, cpu) = policy->max;
-			per_cpu(cpu_cur_freq, cpu) = policy->cur;
+			if (policy->max < info->policy->cur)
+				__cpufreq_driver_target(info->policy, policy->max, CPUFREQ_RELATION_H);
+			else if (policy->min > info->policy->cur)
+				__cpufreq_driver_target(info->policy, policy->min, CPUFREQ_RELATION_L);
 			mutex_unlock(&raw_mutex);
 		break;
 	}
@@ -229,18 +278,34 @@ struct cpufreq_governor cpufreq_gov_raw = {
        .governor               		= cpufreq_governor_raw,
 	   .store_setspeed		   		= cpufreq_raw_set,
 	   .set_frequency 		   		= set_frequency,
+	   .wake_up_kworker 		   	= wake_up_kworker,
        .owner                  		= THIS_MODULE,
 };
 
 static int __init cpufreq_gov_raw_init(void)
 {
-	return cpufreq_register_governor(&cpufreq_gov_raw);
-}
+	int err;
 
+	tarefa_sinalizada = NULL;
+	deadline_tarefa_sinalizada = 0;
+
+	kraw_wq = create_workqueue("kraw");
+	if (!kraw_wq) {
+		printk(KERN_ERR "Creation of kraw failed\n");
+		return -EFAULT;
+	}
+
+	err = cpufreq_register_governor(&cpufreq_gov_raw);
+	if (err)
+		destroy_workqueue(kraw_wq);
+
+	return err;
+}
 
 static void __exit cpufreq_gov_raw_exit(void)
 {
 	cpufreq_unregister_governor(&cpufreq_gov_raw);
+	destroy_workqueue(kraw_wq);
 }
 
 MODULE_AUTHOR("Rawlinson <rawlinson.goncalves@gmail.com>");
